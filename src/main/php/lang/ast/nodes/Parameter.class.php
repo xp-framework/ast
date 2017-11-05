afe74ffb3d45c3422b47951ba56677f78f89d895
Timm Friebe <thekid@thekid.de>
2017-11-05 18:37:31 +0100

    Fix default value for promote

--- src/main/php/lang/ast/nodes/Parameter.class.php
+++ src/main/php/lang/ast/nodes/Parameter.class.php
@@ -3,7 +3,7 @@
 class Parameter extends Annotated {
   public $name, $reference, $type, $variadic, $promote, $default, $annotations;
 
-  public function __construct($name, $type, $default= null, $reference= false, $variadic= false, $promote= false, $annotations= []) {
+  public function __construct($name, $type, $default= null, $reference= false, $variadic= false, $promote= null, $annotations= []) {
     $this->name= $name;
     $this->type= $type;
     $this->default= $default;
