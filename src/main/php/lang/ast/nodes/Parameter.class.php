ba77caa6897b1ef68e6ff87eb6523a5509f25c75
Timm Friebe <thekid@thekid.de>
2017-11-05 18:30:55 +0100

    Change constructir parameter order to be more intuitive

--- src/main/php/lang/ast/nodes/Parameter.class.php
+++ src/main/php/lang/ast/nodes/Parameter.class.php
@@ -3,13 +3,13 @@
 class Parameter extends Annotated {
   public $name, $reference, $type, $variadic, $promote, $default, $annotations;
 
-  public function __construct($name, $reference, $type, $variadic, $promote, $default, $annotations) {
+  public function __construct($name, $type, $default= null, $reference= false, $variadic= false, $promote= false, $annotations= []) {
     $this->name= $name;
-    $this->reference= $reference;
     $this->type= $type;
+    $this->default= $default;
+    $this->reference= $reference;
     $this->variadic= $variadic;
     $this->promote= $promote;
-    $this->default= $default;
     $this->annotations= $annotations;
   }
 }
