02a4faf3843319c3efee04c6c49668d9107183af
Timm Friebe <thekid@thekid.de>
2017-11-05 15:59:59 +0100

    QA: Add api documentation

--- src/main/php/lang/ast/Node.class.php
+++ src/main/php/lang/ast/Node.class.php
@@ -6,10 +6,16 @@ class Node implements Element, \lang\Value {
   public $symbol;
   public $value= null, $kind= null, $line= null;
 
+  /** @param lang.ast.Symbol */
   public function __construct(Symbol $symbol) {
     $this->symbol= $symbol;
   }
 
+  /**
+   * NUD - null denotation
+   *
+   * @return lang.ast.Node
+   */
   public function nud() {
     return $this->symbol->nud
       ? $this->symbol->nud->__invoke($this)
@@ -17,6 +23,12 @@ class Node implements Element, \lang\Value {
     ;
   }
 
+  /**
+   * LED - left denotation
+   *
+   * @param  lang.ast.Node $left
+   * @return lang.ast.Node
+   */
   public function led($left) {
     return $this->symbol->led
       ? $this->symbol->led->__invoke($this, $left)
@@ -24,19 +36,32 @@ class Node implements Element, \lang\Value {
     ;
   }
 
+  /**
+   * STD - statement denotation
+   *
+   * @return lang.ast.Node
+   */
   public function std() {
     return $this->symbol->std ? $this->symbol->std->__invoke($this) : null;
   }
 
+  /** @return string */
   public function hashCode() {
     return $this->symbol->hashCode().$this->kind.Objects::hashOf($this->value);
   }
 
+  /** @return string */
   public function toString() {
     return nameof($this).'(kind= '.$this->kind.', value= '.Objects::stringOf($this->value).')';
   }
 
-  public function compareTo($that) {
-    return 1; // TBI
+  /**
+   * Compares this node to another given value
+   *
+   * @param  var $value
+   * @return int
+   */
+  public function compareTo($value) {
+    return $value === $this ? 0 : 1;
   }
 }
