6f54522a7e530dc19959fb278413feb2bfcae714
Timm Friebe <thekid@thekid.de>
2017-11-06 19:51:04 +0100

    Adjust tests

--- src/test/php/lang/ast/unittest/AnnotationsTest.class.php
+++ src/test/php/lang/ast/unittest/AnnotationsTest.class.php
@@ -1,20 +1,20 @@
 <?php namespace lang\ast\unittest;
 
-use lang\ast\nodes\ClassType;
+use lang\ast\nodes\ClassDeclaration;
 use lang\ast\nodes\Annotation;
 
 class AnnotationsTest extends \unittest\TestCase {
 
   #[@test]
   public function no_annotation() {
-    $this->assertNull((new ClassType([], 'Test', null, [], [], []))->annotation('value'));
+    $this->assertNull((new ClassDeclaration([], 'Test', null, [], [], []))->annotation('value'));
   }
 
   #[@test]
   public function annotation_without_value() {
     $this->assertEquals(
       new Annotation('value', null),
-      (new ClassType([], 'Test', null, [], [], ['value' => null]))->annotation('value')
+      (new ClassDeclaration([], 'Test', null, [], [], ['value' => null]))->annotation('value')
     );
   }
 }
