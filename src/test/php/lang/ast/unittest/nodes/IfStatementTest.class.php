4e626c512ff2d2bff208f914f50f37b54a49efaf
Timm Friebe <thekid@thekid.de>
2019-09-22 18:50:03 +0200

    Simplify test code

--- src/test/php/lang/ast/unittest/nodes/IfStatementTest.class.php
+++ src/test/php/lang/ast/unittest/nodes/IfStatementTest.class.php
@@ -4,39 +4,43 @@ use lang\ast\nodes\IfStatement;
 use lang\ast\nodes\Variable;
 
 class IfStatementTest extends NodeTest {
+  private $condition;
+
+  /** @return void */
+  public function setUp() {
+    $this->condition= new Variable('condition');
+  }
 
   #[@test]
   public function can_create() {
-    new IfStatement(new Variable('condition'), [], null);
+    new IfStatement($this->condition, [], null);
   }
 
   #[@test]
   public function expression() {
-    $condition= new Variable('condition');
-    $this->assertEquals($condition, (new IfStatement(new Variable('condition'), [], null))->expression);
+    $this->assertEquals($this->condition, (new IfStatement($this->condition, [], null))->expression);
   }
 
   #[@test]
   public function body() {
     $body= [$this->returns('true')];
-    $this->assertEquals($body, (new IfStatement(new Variable('condition'), $body, null))->body);
+    $this->assertEquals($body, (new IfStatement($this->condition, $body, null))->body);
   }
 
   #[@test]
   public function otherwise() {
     $otherwise= [$this->returns('true')];
-    $this->assertEquals($otherwise, (new IfStatement(new Variable('condition'), [], $otherwise))->otherwise);
+    $this->assertEquals($otherwise, (new IfStatement($this->condition, [], $otherwise))->otherwise);
   }
 
   #[@test]
   public function children() {
-    $condition= new Variable('condition');
     $body= [$this->returns('true')];
     $otherwise= [$this->returns('false')];
 
     $this->assertEquals(
-      array_merge([$condition], $body, $otherwise),
-      iterator_to_array((new IfStatement($condition, $body, $otherwise))->children())
+      array_merge([$this->condition], $body, $otherwise),
+      iterator_to_array((new IfStatement($this->condition, $body, $otherwise))->children())
     );
   }
 }
