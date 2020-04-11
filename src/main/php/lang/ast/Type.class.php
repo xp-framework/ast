b84d3f3ff13e91eae43fa256c2089b7a1506ef19
Timm Friebe <thekid@thekid.de>
2020-04-11 22:46:07 +0200

    Use <=> instead of strcmp

--- src/main/php/lang/ast/Type.class.php
+++ src/main/php/lang/ast/Type.class.php
@@ -38,6 +38,6 @@ class Type implements Value {
    * @return int
    */
   public function compareTo($value) {
-    return $value instanceof self ? strcmp($this->name(), $value->name()) : 1;
+    return $value instanceof self ? $this->name() <=> $value->name() : 1;
   }
 }
