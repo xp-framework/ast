87c9bba7d64c6ef9dc492cb87035136f26439e7f
Timm Friebe <thekid@thekid.de>
2020-11-28 18:25:52 +0100

    Add test for match expressions

---
+++ src/test/php/lang/ast/unittest/parse/MatchExpressionTest.class.php
@@ -0,0 +1,71 @@
+<?php namespace lang\ast\unittest\parse;
+
+use lang\ast\nodes\{MatchExpression, MatchCondition, Literal, Variable};
+use unittest\{Assert, Test};
+
+class MatchExpressionTest extends ParseTest {
+
+  #[Test]
+  public function empty_match() {
+    $this->assertParsed(
+      [new MatchExpression(new Literal('true', self::LINE), [], null, self::LINE)],
+      'match (true) { };'
+    );
+  }
+
+  #[Test]
+  public function match() {
+    $cases= [
+      new MatchCondition([new Literal('0', self::LINE)], new Literal('true', self::LINE)),
+      new MatchCondition([new Literal('1', self::LINE)], new Literal('false', self::LINE))
+    ];
+    $this->assertParsed(
+      [new MatchExpression(new Variable('arg', self::LINE), $cases, null, self::LINE)],
+      'match ($arg) { 0 => true, 1 => false };'
+    );
+  }
+
+  #[Test]
+  public function match_without_condition() {
+    $this->assertParsed(
+      [new MatchExpression(null, [], null, self::LINE)],
+      'match { };'
+    );
+  }
+
+  #[Test]
+  public function match_with_trailing_comma() {
+    $cases= [
+      new MatchCondition([new Literal('0', self::LINE)], new Literal('true', self::LINE)),
+      new MatchCondition([new Literal('1', self::LINE)], new Literal('false', self::LINE))
+    ];
+    $this->assertParsed(
+      [new MatchExpression(new Variable('arg', self::LINE), $cases, null, self::LINE)],
+      'match ($arg) { 0 => true, 1 => false, };'
+    );
+  }
+
+  #[Test]
+  public function match_with_multiple_cases() {
+    $cases= [
+      new MatchCondition([new Literal('0', self::LINE), new Literal('1', self::LINE)], new Literal('true', self::LINE)),
+      new MatchCondition([new Literal('2', self::LINE), new Literal('3', self::LINE)], new Literal('false', self::LINE))
+    ];
+    $this->assertParsed(
+      [new MatchExpression(new Variable('arg', self::LINE), $cases, null, self::LINE)],
+      'match ($arg) { 0, 1 => true, 2, 3 => false };'
+    );
+  }
+
+  #[Test]
+  public function match_with_default() {
+    $cases= [
+      new MatchCondition([new Literal('0', self::LINE)], new Literal('true', self::LINE)),
+      new MatchCondition([new Literal('1', self::LINE)], new Literal('false', self::LINE))
+    ];
+    $this->assertParsed(
+      [new MatchExpression(new Variable('arg', self::LINE), $cases, new Literal('null', self::LINE), self::LINE)],
+      'match ($arg) { 0 => true, 1 => false, default => null };'
+    );
+  }
+}
