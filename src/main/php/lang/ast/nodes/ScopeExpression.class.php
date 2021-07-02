fcacbf1ed55952d98e2ab34e41ddab304421c062
Timm Friebe <thekid@thekid.de>
2021-07-02 22:16:58 +0200

    Yield variable types of scopes

--- src/main/php/lang/ast/nodes/ScopeExpression.class.php
+++ src/main/php/lang/ast/nodes/ScopeExpression.class.php
@@ -13,6 +13,7 @@ class ScopeExpression extends Node {
   }
 
   /** @return iterable */
-  public function children() { return [$this->member]; }
-
+  public function children() {
+    return $this->type instanceof parent ? [$this->type, $this->member] : [$this->member];
+  }
 }
