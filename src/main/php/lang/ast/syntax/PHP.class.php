7368aa7697ff59d8623764d9593a1a3321c8032c
Timm Friebe <thekid@thekid.de>
2025-02-08 10:01:57 +0100

    Adhere to https://wiki.php.net/rfc/pipe-operator-v3#precedence

--- src/main/php/lang/ast/syntax/PHP.class.php
+++ src/main/php/lang/ast/syntax/PHP.class.php
@@ -175,12 +175,12 @@ class PHP extends Language {
       return new ScopeExpression($scope, $expr, $token->line);
     });
 
-    $this->infix('|>', 90, function($parse, $token, $left) {
-      return new PipeExpression($left, $this->expression($parse, 90), $left->line);
+    $this->infix('|>', 20, function($parse, $token, $left) {
+      return new PipeExpression($left, $this->expression($parse, 20), $left->line);
     });
 
-    $this->infix('?|>', 90, function($parse, $node, $left) {
-      $value= new PipeExpression($left, $this->expression($parse, 90), $left->line);
+    $this->infix('?|>', 20, function($parse, $node, $left) {
+      $value= new PipeExpression($left, $this->expression($parse, 20), $left->line);
       $value->kind= 'nullsafepipe';
       return $value;
     });
