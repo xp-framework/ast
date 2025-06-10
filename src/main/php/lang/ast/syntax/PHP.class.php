ddc6edd4ed9417e12e0c585902dca41ea13592b7
Timm Friebe <thekid@thekid.de>
2025-06-10 20:09:52 +0200

    Create callable helper to disambiguate `f(...)` from `f(...$it)`

--- src/main/php/lang/ast/syntax/PHP.class.php
+++ src/main/php/lang/ast/syntax/PHP.class.php
@@ -154,18 +154,8 @@ class PHP extends Language {
       if ('(' === $parse->token->value) {
         $parse->expecting('(', 'invoke expression');
 
-        // Resolve ambiguity by looking ahead: `func(...)` which is a first-class
-        // callable reference vs. `func(...$it)` - a call with an unpacked argument
-        if ('...' === $parse->token->value) {
-          $dots= $parse->token;
-          $parse->forward();
-          if (')' === $parse->token->value) {
-            $parse->forward();
-            return new CallableExpression(new ScopeExpression($scope, $expr, $token->line), $left->line);
-          }
-
-          array_unshift($parse->queue, $parse->token);
-          $parse->token= $dots;
+        if ($this->callable($parse)) {
+          return new CallableExpression(new ScopeExpression($scope, $expr, $token->line), $token->line);
         }
 
         $arguments= $this->arguments($parse);
@@ -190,16 +180,8 @@ class PHP extends Language {
 
       // Resolve ambiguity by looking ahead: `func(...)` which is a first-class
       // callable reference vs. `func(...$it)` - a call with an unpacked argument
-      if ('...' === $parse->token->value) {
-        $dots= $parse->token;
-        $parse->forward();
-        if (')' === $parse->token->value) {
-          $parse->forward();
-          return new CallableExpression($left, $left->line);
-        }
-
-        array_unshift($parse->queue, $parse->token);
-        $parse->token= $dots;
+      if ($this->callable($parse)) {
+        return new CallableExpression($left, $token->line);
       }
 
       $arguments= $this->arguments($parse);
@@ -299,6 +281,10 @@ class PHP extends Language {
       // clone $x vs. clone($x) or clone($x, ["id" => 6100])
       if ('(' === $parse->token->value) {
         $parse->forward();
+        if ($this->callable($parse)) {
+          return new CallableExpression(new Literal('clone', $token->line), $token->line);
+        }
+
         $arguments= $this->arguments($parse);
         $parse->expecting(')', 'clone arguments');
       } else {
@@ -353,25 +339,16 @@ class PHP extends Language {
 
       // Resolve ambiguity by looking ahead: `new T(...)` which is a first-class
       // callable reference vs. `new T(...$it)` - a call with an unpacked argument
-      if ('...' === $parse->token->value) {
-        $dots= $parse->token;
-        $parse->forward();
-        if (')' === $parse->token->value) {
-          $parse->forward();
-
-          if (null === $type) {
-            $class= $this->class($parse, null);
-            $class->annotations= $annotations;
-            $new= new NewClassExpression($class, null, $token->line);
-          } else {
-            $new= new NewExpression($type, null, $token->line);
-          }
-
-          return new CallableNewExpression($new, $token->line);
+      if ($this->callable($parse)) {
+        if (null === $type) {
+          $class= $this->class($parse, null);
+          $class->annotations= $annotations;
+          $new= new NewClassExpression($class, null, $token->line);
+        } else {
+          $new= new NewExpression($type, null, $token->line);
         }
 
-        array_unshift($parse->queue, $parse->token);
-        $parse->token= $dots;
+        return new CallableNewExpression($new, $token->line);
       }
 
       $arguments= $this->arguments($parse);
@@ -1737,6 +1714,22 @@ class PHP extends Language {
     $parse->expecting('}', 'class');
 
     return $decl;
+  }
+
+  public function callable($parse) {
+    if ('...' === $parse->token->value) {
+      $dots= $parse->token;
+      $parse->forward();
+      if (')' === $parse->token->value) {
+        $parse->forward();
+        return true;
+      }
+
+      // Not first-class callable syntax but unpack
+      array_unshift($parse->queue, $parse->token);
+      $parse->token= $dots;
+    }
+    return false;
   }
 
   public function arguments($parse) {
